eebe9eb7d4799d97b9ac65b0267f40bd1cd3cb42
saranyabaskaran90 <32703797+saranyabaskaran90@users.noreply.github.com>
2017-10-16 00:58:44 +0530

    Add files via upload

---
+++ .gitignore/app/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-DOM');
+require('./index.css');
+var App = require('./components/App');
+
+
+
+ReactDOM.render(
+	<App />, document.getElementById('app'));
+
